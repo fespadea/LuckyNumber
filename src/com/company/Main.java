8c8919ffe2c1bf0a5cc99c927da5b20dde969691
Fernando Spadea <fespadearocks@gmail.com>
2016-10-13 22:25:32 -0400

    <You're Lucky Commit Message Is...>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,21 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 27;
-        ArrayList nameList;
+        String x = "2";
+        String nameList = "";
         do {
-            System.out.println("Give me a number.");
+            System.out.println("Give me a name.");
             Scanner giveMeName = new Scanner(System.in);
             String name = giveMeName.nextLine();
             perfectRandom person = new perfectRandom(name);
-            System.out.println("ice to meet you " + name + "! Your lucky number is " + person.luckyNumber + ".");
-            nameList.add(person.name + ": " + person.luckyNumber + "\n");
-        }while(x != 0);
+            System.out.println("Nice to meet you " + name + "! Your lucky number is " + person.luckyNumber + ".");
+            nameList = nameList + (String.valueOf(person.name)) + ": " + (String.valueOf(person.luckyNumber)) + "\n";
+            System.out.println("What next?");
+            Scanner number = new Scanner(System.in);
+            x = number.next();
+            if(x.equals("1")){
+                System.out.print(nameList);
+            }
+        }while(x.equals("2"));
     }
 }
